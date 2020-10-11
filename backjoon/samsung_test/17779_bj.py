0f129aff6c78d1030f871240bd8279686fd58f54
kywkyw <1002ever@naver.com>
2020-10-11 22:42:30 +0900

    [+] 17779_bj

--- backjoon/samsung_test/17779_bj.py
+++ backjoon/samsung_test/17779_bj.py
@@ -1,6 +1,5 @@
-# # 백준 17779 게리맨더링2 - 삼성 코테 기출
+# 백준 17779 게리맨더링2 - 삼성 코테 기출
 
-# 테
 dx = [1, 1, -1, -1]
 dy = [-1, 1, 1, -1]
 
@@ -9,6 +8,7 @@ frame = [[] for _ in range(n)]
 part_frame = []
 
 reg_cnt = [0]*5
+ans = 2147000000
 
 for i in range(n):
     frame[i] = list(map(int, input().split()))
@@ -22,6 +22,51 @@ for x in range(n-2):
                 if y-d1+d2 < 0 or y-d1+d2 >= n or x+d1+d2 >= n or y+d2 >= n or x+d2 >= n:
                     break
 
+                imp = 0
                 part_frame = [[0]*n for _ in range(n)]
-                for dx in range(1, d1+1):
-                    for dy in range(1, )
+                mx, my = x, y
+                for i in range(4):
+                    if i % 2 == 0:
+                        dir = d1
+                    else:
+                        dir = d2
+                    for j in range(dir):
+                        mx = mx + dx[i]
+                        my = my + dy[i]
+                        if mx < 0 or mx >= n or my < 0 or my >= n:
+                            imp = 1
+                            break
+                        part_frame[mx][my] = 5
+                    if imp == 1:
+                        break
+                if imp == 1:
+                    continue
+                for i in range(n):
+                    if part_frame[i].count(5) == 2:
+                        for j in range(part_frame[i].index(5)+1, n):
+                            if part_frame[i][j] == 0:
+                                part_frame[i][j] = 5
+                            else:
+                                break
+                
+                for i in range(n):
+                    for j in range(n):
+                        if part_frame[i][j] != 5:
+                            if i < x+d1 and j <= y:
+                                part_frame[i][j] = 1
+                            elif i <= x+d2 and y < j < n:
+                                part_frame[i][j] = 2
+                            elif x+d1 <= i < n and j < y-d1+d2:
+                                part_frame[i][j] = 3
+                            else:
+                                part_frame[i][j] = 4
+                
+                reg_cnt = [0]*5
+                for i in range(n):
+                    for j in range(n):
+                        cur = part_frame[i][j] - 1
+                        reg_cnt[cur] += frame[i][j]
+                if max(reg_cnt) - min(reg_cnt) < ans:
+                    ans = max(reg_cnt) - min(reg_cnt)
+
+print(ans)
