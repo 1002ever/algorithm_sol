37cc6e03604646335dd24a5ef417c8573ba50eea
kywkyw <1002ever@naver.com>
2020-11-27 22:21:41 +0900

    test

--- baekjoon/samsung_A/17135_bj.py
+++ baekjoon/samsung_A/17135_bj.py
@@ -1,93 +1,68 @@
-# 삼성 A형 테스트 기출 - 17135 캐슬 디펜스
-
 from itertools import combinations
-from collections import deque
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-ans = -2147000000
-tmp_ans = 0
+# 궁수가 공격할 적 선택 후 그를 list로 return
+def select_enemy(enemies, archers, x, d):
 
-def endChk(frame, n):
-    tmp_sum = 0
-    for i in range(n):
-        tmp_sum += sum(frame[i])
-        if tmp_sum > 0:
-            return True
-    return False
+    enemy_idx_set = set()
 
-def bfs(frame, xy, d):
-    global tmp_ans
+    # 궁수 순회
+    for archer in archers:
 
-    q = deque()
-    visited = [[0]*m for _ in range(n)]
-    cnt = 0
-    x, y = xy
-    q.appendleft((x, y))
+        # 지금까지 가장 가까운 적 최소 거리
+        dist = 2147000000
+        # 지금까지 가장 가까운 적 인덱스
+        enemy_idx = -1
 
-    cand_xy = []
-    while cnt < d and q:
-        cnt += 1
-        x, y = q.pop()
-        for i in range(4):
-            mx = x + dx[i]
-            my = y + dy[i]
-            if mx < 0 or mx >= n or my < 0 or my >= n:
-                continue
-            if visited[mx][my] == 1:
-                continue
-            if frame[mx][my] == 1:
-                cand_xy.append((mx, my))
-            q.appendleft((mx, my))
-            visited[mx][my] = 1
-        if cand_xy:
-            break
-    if len(cand_xy) == 1:
-        return cand_xy[0]
-    elif len(cand_xy) > 1:
-        cand_xy = sorted(cand_xy, key = lambda x : x[1])
-        return cand_xy[0]
-    else:
-        return -1
-
-def dfs(frame, cand, d, n, m):
-    global ans
-    global tmp_ans
-
-    tmp_ans = 0
-    while endChk(frame, n):
-        del_xy = []
-        for y in cand:
-            xy = [n, y]
-            del_xy.append(bfs(frame, xy, d))
-        for xy in del_xy:
-            if xy == -1:
-                continue
-            else:
-                x, y = xy
-                frame[x][y] = 0
-                tmp_ans += 1
-        # 아래로 이동
-        for i in range(n):
-            for j in range(m):
-                if frame[i][j] == 1:
-                    frame[i][j] = 0
-                    if i != n-1:
-                        frame[i+1][j] = 1
+        #  적 순회하며 dist, enemy_idx 갱신
+        for idx, enemy in enumerate(enemies):
+            tmp_dist = abs(enemy[0] - x) +  abs(enemy[1] - archer)
+            # 사정거리 안쪽이면 고려해보기
+            if d >= tmp_dist:
+                # 지금까지 가장 가까운 거리이면 갱신
+                if dist > tmp_dist:
+                    dist = tmp_dist
+                    enemy_idx = idx
+                # 거리가 같다면 y 비교가 필요
+                if dist == tmp_dist:
+                    # 현재 y가 더 작다면 갱신
+                    if enemies[idx][1] < enemies[enemy_idx][1]:
+                        enemy_idx = idx
         
-        print(frame)
+        if enemy_idx != -1:
+            enemy_idx_set.add(enemy_idx)
 
-    if tmp_ans > ans:
-        ans = tmp_ans
-    
+    enemy_idx_list = list(enemy_idx_set)
+    enemy_idx_list.sort(reverse=True) 
+    return enemy_idx_list
+
+
+
 n, m, d = map(int, input().split())
-frame = [[] for _ in range(n+1)]
-frame[n] = [0]*m
+frame = [[] for _ in range(n)]
+enemies = []
+
 for i in range(n):
     frame[i] = list(map(int, input().split()))
+    for j in range(m):
+        if frame[i][j] == 1:
+            enemies.append([i, j])
 
-cand_y = range(m)
-for cand in combinations(cand_y, 3):
-    dfs(frame, list(cand), d, n, m)
+archers = combinations(range(m), 3)
 
-print(ans)
+# 궁수 경우의 수를 모두 순회
+for archer_case in archers:
+
+    tmp_enemies = [[] for _ in range(len(enemies))]
+    for i in range(len(enemies)):
+        tmp_enemies[i] = enemies[i][:]
+
+    # 해당 케이스에 대해서 제거할 적이 없을 때까지
+    while tmp_enemies:
+        # 1. 제거 대상 선정
+        enemy_list = select_enemy(tmp_enemies, archer_case, n, d)
+
+        # 2. 제거
+        for idx in enemy_list:
+            tmp_enemies.pop(idx)
+
+        # 3. 이동
